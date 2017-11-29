61dab86b1cec4508e792cdb52662fd88f6954207
Igor <igorfarias201@gmail.com>
2017-11-29 14:42:11 -0300

    fim do marco 2

--- src/beans/Usuario.java
+++ src/beans/Usuario.java
@@ -5,14 +5,6 @@ import java.util.ArrayList;
  * Algumas dúvidas:
  * - Nos métodos setters o código recebe a senha como argumento, 
  * 		seria melhor o próprio metodo pedir essa senha para verificá-la?
- * - Dúvida com o retorno de getAmigos() e de procurarAmigo():
- * 		deixo void e printo na tela ou deixo String?
- * 
- * 
- * - Nos metodos getters eu verifico se o perfil é privado, e apenas caso não seja,
- * o metodo retornará o devido atributo. 
- * O problema é caso  perfil seja privado nem mesmo as classes DAO serão capazes de acessar os atributos.
- * 
  */
 
 public class Usuario {
@@ -23,8 +15,6 @@ public class Usuario {
 	private String primeiroNome;
 	private String sobrenome;
 	private String profissao;
-//	private ArrayList<Usuario> amigos = new ArrayList<Usuario>();
-	private boolean perfilPrivado;
 
 	public Usuario() {
 		
@@ -40,56 +30,9 @@ public class Usuario {
 		this.profissao = profissao;
 		
 	}
-	
-/*	public void adicionarAmigo(Usuario amigo) {
-		this.amigos.add(amigo);
-		System.out.println("Amigo adicionado!");
-	}
-
-	public void desfazerAmigo(Usuario amigo) {
-		this.amigos.remove(amigo);
-		System.out.println(amigo.primeiroNome + " foi removido.");
-	}
-	
-	// Procura um amigo dentre os amigos do usuario
-	
-	// Tô achando esse método bastante ilegível, eu poderia melhorá-lo?
-	public void procurarAmigo(String nome) {
-		ArrayList<String> encontrados = new ArrayList<>();
-		for (Usuario x : amigos) {
-			if(x.primeiroNome.contains(nome) || x.sobrenome.contains(nome)) {
-				encontrados.add(x.toString());
-			}
-			if(x.login.contains(nome)) {
-				 
-				if (!encontrados.contains(x.toString())) {
-					encontrados.add(x.toString());
-				}
-			}
-		}
-		System.out.println(encontrados);
-	}
-*/
-	
-	public void setPerfilPrivado(boolean perfilPrivado, String senha) {
-		if (senha == this.senha) {
-			this.perfilPrivado = perfilPrivado;
-		} else {
-			System.out.println("Senha incorreta. Permissão negada.");
-		}
-	}
-
-	public boolean isPerfilPrivado() {
-		return perfilPrivado;
-	}
 
 	public String getEmail() {
-		if(!isPerfilPrivado()) {
 			return email;
-		} else {
-			System.out.println("Permissão negada.");
-			return null;
-		}
 	}
 	
 	public void setEmail(String email, String senha) {
@@ -101,12 +44,7 @@ public class Usuario {
 	}
 	
 	public String getPrimeiroNome() {
-		if(!isPerfilPrivado()) {
 			return primeiroNome;
-		} else {
-			System.out.println("Permissão negada.");
-			return null;
-		}
 	}
 
 	public void setPrimeiroNome(String primeiroNome, String senha) {
@@ -118,12 +56,7 @@ public class Usuario {
 	}
 
 	public String getSobrenome() {
-		if(!isPerfilPrivado()) {
 			return sobrenome;
-		} else {
-			System.out.println("Permissão negada.");
-			return null;
-		}
 	}
 
 	public void setSobrenome(String sobrenome, String senha) {
@@ -135,12 +68,7 @@ public class Usuario {
 	}
 
 	public String getProfissao() {
-		if(!isPerfilPrivado()) {
 			return profissao;
-		} else {
-			System.out.println("Permissão negada.");
-			return null;
-		}
 	}
 
 	public void setProfissao(String profissao, String senha) {
@@ -162,17 +90,6 @@ public class Usuario {
 			System.out.println("Senha incorreta. Permissão negada.");
 		}
 	}
-	
-/*	public ArrayList<Usuario> getAmigos() {
-		if(!isPerfilPrivado()) {
-			System.out.println("Amigos de " + primeiroNome);
-			System.out.println(amigos);
-			return amigos;
-		} else {
-			System.out.println("Permissão negada.");
-			return null;
-		}
-	}*/
 	
 	public String getSenha() {
 		return senha;
