3b4aa923a9e37a5432c563052dd3dfc7cac07bc4
igorfarias1 <32603991+igorfarias1@users.noreply.github.com>
2017-12-06 10:21:10 -0200

    Create Facade.java

---
+++ src/ui/Facade.java
@@ -0,0 +1,35 @@
+
+package ui;
+
+import java.util.Scanner;
+import beans.*;
+import controllers.*;
+
+public class Facade {
+
+	public void cadastrarUsuario() {
+
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Criação de usuário!");
+		System.out.print("Nome: ");
+		String nome = leitor.nextLine();
+		System.out.print("Sobrenome: ");
+		String sobrenome = leitor.nextLine();
+		System.out.print("Login: ");
+		String login = leitor.nextLine();
+		System.out.print("Senha: ");
+		String senha = leitor.nextLine();
+		System.out.print("Email: ");
+		String email = leitor.nextLine();
+		System.out.print("Profissão: ");
+		String profissao = leitor.nextLine();
+
+		Usuario u = new Usuario(login, senha, email, nome, sobrenome, profissao);
+
+		DAOUsuario integrador = new DAOUsuario();
+
+		integrador.criarUsuario(u);
+
+	}
+
+}
