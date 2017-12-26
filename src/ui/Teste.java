5f752d1d80b8b278860618695d51829d786b2ec7
Igor <igorfarias201@gmail.com>
2017-12-26 10:13:51 -0300

    26-12-17

--- src/ui/Teste.java
+++ src/ui/Teste.java
@@ -3,13 +3,25 @@ package ui;
 import java.util.Scanner;
 import beans.*;
 import controllers.*;
-import java.util.ArrayList;
+
 
 public class Teste {
 
 	public static void main(String[] args) {
-		Usuario u1 = new Usuario("igur", "123", "igo", "igor", "farias", "pedreiro");
-		System.out.println(u1);
+		
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		String texto = "";
+//		String linha = "";
+//		
+//		System.out.println("Digite seu texto: (dê enter e digite -- para finalizar) ");
+//		while(linha != "--") {
+//			linha = leitor.nextLine();
+//			//texto = texto + "\n" + linha;
+//			System.out.println(texto);
+//		}
+		
 		
 	}
 	
